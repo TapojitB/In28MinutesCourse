7c49db42fef56de321b0c479c366c948a28c21bd
Tapojit Bhattacharya <tapojit.bhattacharya@gmail.com>
2018-09-27 14:04:35 +0530

    Hystrix incorporated

--- limits-service/src/main/java/com/in28minutes/microservices/limitsservice/LimistConfigurationController.java
+++ limits-service/src/main/java/com/in28minutes/microservices/limitsservice/LimistConfigurationController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.in28minutes.microservices.limitsservice.bean.LimitConfiguration;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
 /**
  * @author TapojitBhattacharya
@@ -22,6 +23,18 @@ public class LimistConfigurationController {
 	@GetMapping("/limits")
 	public LimitConfiguration retrieveLimitsFromConfigurations() {
 		return new LimitConfiguration(configuration.getMaximum(), configuration.getMinimum());
+
+	}
+
+	@GetMapping("/limits-fault-tolerance")
+	@HystrixCommand(fallbackMethod = "fallbackRetrieveConfigurations")
+	public LimitConfiguration retrieveConfigurations() {
+		throw new RuntimeException("Runtime Exception Ocured");
+
+	}
+
+	public LimitConfiguration fallbackRetrieveConfigurations() {
+		return new LimitConfiguration(999, 9);
 
 	}
 
